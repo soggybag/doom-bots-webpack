1281b10fdf333c42defd06baff0bbe37c9e8c7f9
Mitchell <soggybag@gmail.com>
2022-09-27 22:03:23 -0700

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,30 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-
-    entry: './src/index.js',
-
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/build/',
-        filename: 'project.bundle.js'
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/build/',
+    filename: 'project.bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/'),
     },
-
-    module: {
-        rules: [
-          {
-            test: [ /\.vert$/, /\.frag$/ ],
-            use: 'raw-loader'
-          }
-        ]
-    },
-
-    plugins: [
-        new webpack.DefinePlugin({
-            'CANVAS_RENDERER': JSON.stringify(true),
-            'WEBGL_RENDERER': JSON.stringify(true)
-        })
-    ]
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: 'raw-loader',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true),
+    }),
+  ],
 
 };
